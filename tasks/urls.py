3079a48f99de7fa6d5f79a991639038ea5ced0cc
MarceloCMiguel <marcelocmiguel@outlook.com>
2021-11-20 23:47:56 -0300

    serializer e request get feito

--- tasks/urls.py
+++ tasks/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('get_tasks', views.task_list, name='get_tasks'),
+
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,JsonResponse
 
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from tasks.serializer import TaskSerializer
+from tasks.models import Task
 
 # Create your views here.
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the tasks index.")
+
+@api_view(['GET'])
+def task_list(request):
+    """
+    List all tasks.
+    """
+    if request.method == 'GET':
+        tasks = Task.objects.all()
+        serializer = TaskSerializer(tasks, many=True)
+        return JsonResponse(serializer.data,status=200, safe=False)
