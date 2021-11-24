1e19ef201f5b711cf44acf1a3b93ab3b73b1b84c
MarceloCMiguel <marcelocmiguel@outlook.com>
2021-11-24 11:42:22 -0300

    post

--- tasks/urls.py
+++ tasks/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('get_tasks', views.task_list, name='get_tasks'),
+    path('post_task',views.post_task, name='post_task')
 
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,18 +1,19 @@
-from django.shortcuts import render
+import re
+from django.shortcuts import render, redirect
 from django.http import HttpResponse,JsonResponse
 
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-
+from rest_framework.parsers import JSONParser
 from tasks.serializer import TaskSerializer
 from tasks.models import Task
 
 # Create your views here.
 
-
 def index(request):
     return HttpResponse("Hello, world. You're at the tasks index.")
+
 
 @api_view(['GET'])
 def task_list(request):
@@ -23,3 +24,18 @@ def task_list(request):
         tasks = Task.objects.all()
         serializer = TaskSerializer(tasks, many=True)
         return JsonResponse(serializer.data,status=200, safe=False)
+    if request.method == 'POST':
+        return render(request,'forms.html')
+
+
+def post_task(request):  
+    if request.method == "POST":  
+        form = TaskSerializer(data=request.POST)
+        if form.is_valid():  
+            try:  
+                form.save()  
+                return redirect('/tasks/get_tasks')  
+            except Exception as e:  
+                print(e) 
+    return render(request,'forms.html')
+
